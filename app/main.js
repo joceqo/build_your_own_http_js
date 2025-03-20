327416c1acb0ec302bfae2ae158933eacbbbbd4f
jocelin queau <queau.j@gmail.com>
2025-03-20 14:02:30 +0100

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,4 +1,7 @@
 const net = require("node:net");
+const path = require("node:path")
+const fs = require("node:fs")
+
 const {
 	extractRequestLine,
 	extractRequestLineInfos,
@@ -7,6 +10,9 @@ const {
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
+
+const args = process.argv.slice(2);
+const dir = args[args.indexOf("--directory") + 1];
 
 const server = net.createServer((socket) => {
 	socket.on("close", () => {
@@ -24,13 +30,37 @@ const server = net.createServer((socket) => {
         const headers = extractHeaders(request)
         const userAgent = headers['User-Agent']
         socket.end(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)
-        return
       }
-			if (target.startsWith("/echo/")) {
+			else if (target.startsWith("/echo/")) {
 				const text = target.split("/").at(-1);
 				socket.end(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${text.length}\r\n\r\n${text}`);
-        
-			} else {
+			} else if(target.startsWith("/files")){
+        const doesArgHaveDir =
+					args.findIndex((arg) => arg === "--directory") !== -1;
+
+          if (!doesArgHaveDir) {
+            socket.write("HTTP/1.1 500 Internal Server Error\r\n\r\n");
+            socket.end();
+            return;
+          }
+
+          const fileName = target.split("/files/")[1];
+          const filePath = path.join(dir, fileName);
+
+          const isFileExists = fs.existsSync(filePath);
+				if (!isFileExists) {
+					socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+					socket.end();
+				}
+
+        const file = fs.readFileSync(filePath, "utf8");
+				const fileLength = file.length;
+
+				socket.write(
+					`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileLength}\r\n\r\n${file}`,
+				);
+      }
+      else {
 				socket.end("HTTP/1.1 404 Not Found\r\n\r\n");
 			}
 		}
---
+++ notes/return_a_file.md
@@ -0,0 +1,5 @@
+# Return a file #ap6
+
+we get the directory arg when the program start with `process.argv.slice(2)`
+
+if a directory was provided, we check if the file exist, and return it
